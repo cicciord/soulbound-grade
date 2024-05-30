85e14e60468f38385bdb042332bd1177ca89d430
cicciord <102993057+cicciord@users.noreply.github.com>
2024-05-30 18:07:02 +0200

    opensea metadata format in scripts

--- soulbound-grade-contracts/scripts/assignGradesBatch.ts
+++ soulbound-grade-contracts/scripts/assignGradesBatch.ts
@@ -4,22 +4,41 @@ import grades from "../constants/grade.test.json";
 async function main() {
   const chainId = network.config.chainId || 31337;
 
-  const contractAddress =
-    require(`../ignition/deployments/chain-${chainId}/deployed_addresses.json`)[
-      "SoulboundGradeModule#SoulboundGrade"
-    ];
+  const contractAddress = require(
+    `../ignition/deployments/chain-${chainId}/deployed_addresses.json`,
+  )["SoulboundGradeModule#SoulboundGrade"];
 
   const contract = await viem.getContractAt("SoulboundGrade", contractAddress);
 
   for (const grade of grades) {
+    if (grade.grade < 18 || grade.grade > 31) {
+      throw new Error("The grade must be in the range [18, 31]");
+    }
+
+    const metadata = {
+      description: "Nft Grade for the Blockchain and Cryptoeconomy Course",
+      image: "https://file.didattica.polito.it/download/DSK_CONDIVISO/131376",
+      name: `Soulbound Grade of s${grade.id.toString()}`,
+      attributes: [
+        {
+          display_type: "number",
+          trait_type: "Grade",
+          value: grade.grade,
+          max_value: 31,
+        },
+      ],
+    };
+    const metadata_str = Buffer.from(JSON.stringify(metadata)).toString();
+    const tokenUri = btoa(metadata_str);
+
     console.log(
-      `Assigning grade ${grade.grade} to student s${grade.id} at address ${grade.address}`
+      `Assigning grade ${grade.grade === 31 ? "30L" : grade.grade} to student s${grade.id} at address ${grade.address}`,
     );
 
     await contract.write.safeMint([
       grade.address as `0x${string}`,
       BigInt(grade.id),
-      `Grade: ${grade.grade}`,
+      tokenUri,
     ]);
   }
 }
